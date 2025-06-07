d24ba9934b041164181fe509259ea9221b185936
Nikolay <n.matyushin@tawasal.ae>
2025-06-07 15:56:08 +0300

    fix climate.py

--- components/haier/climate.py
+++ components/haier/climate.py
@@ -1,14 +1,17 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate_ir
-from esphome.const import CONF_ID
 
 AUTO_LOAD = ["climate_ir"]
 
 haier_ns = cg.esphome_ns.namespace("haier")
 HaierClimate = haier_ns.class_("HaierClimate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(HaierClimate)
+CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(HaierClimate),
+    }
+)
 
 
 async def to_code(config):
